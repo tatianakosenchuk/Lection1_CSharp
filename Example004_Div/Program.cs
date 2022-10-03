bc56540d2a62f19f7d472c45937624f737009f7e
Tatyana <tatiana.kosenchuk@gmail.com>
2022-10-03 14:38:38 +0300

    Div

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,5 +1,6 @@
 ﻿double NumberZ = new Random().Next(1,100);
 Console.WriteLine(NumberZ);
-double NumberV = 5;
+double NumberV = new Random().Next(1,10);
+Console.WriteLine(NumberV);
 double result = NumberZ / NumberV;
 Console.WriteLine(result);
